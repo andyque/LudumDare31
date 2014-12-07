4c1bf082c21d8a6dafc50c6d0981a8e9ac9c9945
andyque <guanghui8827@126.com>
2014-12-07 14:46:40 +0800

    add new resource

--- src/resource.js
+++ src/resource.js
@@ -1,10 +1,10 @@
 var res = {
     background_png : "res/images/background.png",
     foreground_png : "res/images/foreground.png",
-    bird_png : "res/images/bird1.png",
+    bird_png : "res/images/bird11.png",
     item1_png : "res/images/rice.png",
     item2_png : "res/images/worm.png",
-    cat_png : "res/images/cat.png",
+    cat_png : "res/images/cat1.png",
     stab_png : "res/images/down_dents.png",
     eagle1_png : "res/images/eagle1.png",
     eagle2_png : "res/images/eagle2.png",
